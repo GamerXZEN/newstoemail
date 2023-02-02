4a0767f5e8d7775dc6ee77e52144c8d2f98a1397
FESL Sapphire Dev <codspecialops@gmail.com>
2023-02-02 15:36:53 -0600

    Send an email with english titles, urls, and descriptions

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from send_email import send_email as se
 
+topic = "tesla"
 api_key = "ab1ef33e9fcc448b86f1e7b0cf03c98f"
-url = "https://newsapi.org/v2/everything?q=" \
-      "tesla&from=2023-01-02&sortBy=publishedAt&apiKey" \
-      "=ab1ef33e9fcc448b86f1e7b0cf03c98f"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}" \
+      "&from=2023-01-02&" \
+      "sortBy=publishedAt" \
+      "&apiKey=ab1ef33e9fcc448b86f1e7b0cf03c98f" \
+      "&language=en" \
 
 request = requests.get(url)
 content = request.json()
@@ -15,8 +19,11 @@ email_addresses = ["testcasepython123@gmail.com", "sujathanelluri@gmail.com",
                    "karthik.saripalli.643@k12.friscoisd.org"]
 
 body = ""
-for article in content["articles"]:
-	body = body + str(article["title"]) + "\n" + str(article["description"]) + "\n\n"
+for article in content["articles"][:20]:
+	if article["title"] is not None:
+		body = "Subject: Today's Morning News" + \
+		body + article["title"] + "\n" + \
+		article["description"] + "\n" + article["url"] + "\n\n"
 
 body = body.encode("utf-8")
 for email_address in email_addresses:
